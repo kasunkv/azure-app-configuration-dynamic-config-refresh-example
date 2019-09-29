92c76e360bda5e9c3b7a192786f00b111d1a9749
Kasun Kodagoda <kvkrusl@gmail.com>
2019-09-29 11:22:52 +0530

    Initial implementation

---
+++ src/AzureAppConfiguration/DynamicRefreshExample/AppSettings.cs
@@ -0,0 +1,13 @@
+﻿namespace DynamicRefreshExample
+{
+    public class AppSettings
+    {
+        public HomePage HomePage { get; set; }
+    }
+
+    public class HomePage
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ src/AzureAppConfiguration/DynamicRefreshExample/Models/HomeViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace DynamicRefreshExample.Models
+{
+    public class HomeViewModel
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
